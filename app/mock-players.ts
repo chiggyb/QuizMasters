ddb38186ea3bde509bb20c4d1444b22b1aaaa78c
amrossetto <arossetto1@gmail.com>
2017-10-13 17:00:15 -0400

    Delete mock-players.ts

--- app/mock-players.ts
+++
@@ -1,8 +0,0 @@
-import { Players } from './Players';
-
-export const PLAYERS: Players[] = [
-  { id: 123, userName: 'John Smith', userPic: 'http://placekitten.com.s3.amazonaws.com/homepage-samples/30/30.jpg' },
-  { id: 234, userName: 'Bridget Jones', userPic: 'http://placekitten.com.s3.amazonaws.com/homepage-samples/30/30.jpg' },
-  { id: 345, userName: 'Kay M', userPic: 'http://placekitten.com.s3.amazonaws.com/homepage-samples/30/30.jpg' },
-     
-];
