efef576506fb59dc570bbe2209c9aaafd538c7ab
amrossetto <arossetto1@gmail.com>
2017-11-16 19:14:16 -0500

    Add files via upload

---
+++ allison/PlayerLobby/PlayerHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace PlayerLobby
+{
+    public class PlayerHub : Hub
+    {
+        public void Send(string name)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name);
+        }
+    }
+}
