3e37613a3bcf78074212ee7b07e1e6a95919f79c
amrossetto <arossetto1@gmail.com>
2017-11-30 19:16:17 -0500

    updated player service

--- allison/player_service/players.component.ts
+++ allison/player_service/players.component.ts
@@ -6,12 +6,12 @@ import { PlayersService } from './players.service';
 @Component({
   selector: 'the-players',
 })
-class playersCheck {
+export class PlayersCheck {
   constructor(private ref: ChangeDetectorRef, private playersService: PlayersService) {
     ref.detach();
     setInterval(() => {
       this.ref.detectChanges();
     }, 5000); 
-	
   }
-}
+	
+	}
